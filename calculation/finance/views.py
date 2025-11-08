5bcdd5d6f9dbdee672e755786cd1488b904cf432
M3 air <m3air@M3s-MacBook-Air.local>
2025-11-08 13:14:49 +0600

    complete total 50 finance calculator

--- calculation/finance/views.py
+++ calculation/finance/views.py
@@ -10,3 +10,157 @@ def net_worth(request):
 
 def dti(request):
     return render(request, "finance/dti.html")
+
+def savings_goal(request):
+    return render(request, "finance/savings_goal.html")
+
+def emergency_fund(request):
+    return render(request, "finance/emergency_fund.html")
+
+def cash_flow(request):
+    return render(request, "finance/cash_flow.html")
+
+def spending_tracker(request):
+    return render(request, "finance/spending_tracker.html")
+
+def inflation_calculator(request):
+    return render(request, "finance/inflation_calculator.html")
+
+def cost_of_living_comparison_calculator(request):
+    return render(request, "finance/cost_of_living_comparison_calculator.html")
+
+def paycheck_calculator(request):
+    return render(request, "finance/paycheck_calculator.html")
+
+def overtime_pay_calculator(request):
+    return render(request, "finance/overtime_pay_calculator.html")
+
+def hourly_to_salary_converter(request):
+    return render(request, "finance/hourly_to_salary_converter.html")
+
+def salary_to_hourly_converter(request):
+    return render(request, "finance/salary_to_hourly_converter.html")
+
+def disposable_income_calculator(request):
+    return render(request, "finance/disposable_income_calculator.html")
+
+def discretionary_income_calculator(request):
+    return render(request, "finance/discretionary_income_calculator.html")
+
+def financial_health_check_calculator(request):
+    return render(request, "finance/financial_health_check_calculator.html")
+
+def bill_splitting_calculator(request):
+    return render(request, "finance/bill_splitting_calculator.html")
+
+def yearly_expense_planner(request):
+    return render(request, "finance/yearly_expense_planner.html")
+
+def sinking_fund_calculator(request):
+    return render(request, "finance/sinking_fund_calculator.html")
+
+def gift_tax_calculator(request):
+    return render(request, "finance/gift_tax_calculator.html")
+
+def allowance_calculator(request):
+    return render(request, "finance/allowance_calculator.html")
+
+def latte_factor_calculator(request):
+    return render(request, "finance/latte_factor_calculator.html")
+
+def expense_report_calculator(request):
+    return render(request, "finance/expense_report_calculator.html")
+
+def personal_inflation_rate_calculator(request):
+    return render(request, "finance/personal_inflation_rate_calculator.html")
+
+def rainy_day_fund_calculator(request):
+    return render(request, "finance/rainy_day_fund_calculator.html")
+
+def fire_calculator(request):
+    return render(request, "finance/fire_calculator.html")
+
+def wage_garnishment_calculator(request):
+    return render(request, "finance/wage_garnishment_calculator.html")
+
+def severance_pay_calculator(request):
+    return render(request, "finance/severance_pay_calculator.html")
+
+def windfall_tax_calculator(request):
+    return render(request, "finance/windfall_tax_calculator.html")
+
+def holiday_budget_calculator(request):
+    return render(request, "finance/holiday_budget_calculator.html")
+
+def charitable_donation_tax_deduction_calculator(request):
+    return render(request, "finance/charitable_donation_tax_deduction_calculator.html")
+
+def moving_cost_calculator(request):
+    return render(request, "finance/moving_cost_calculator.html")
+
+def subscription_management_calculator(request):
+    return render(request, "finance/subscription_management_calculator.html")
+
+
+def personal_loan_comparison_calculator(request):
+    return render(request, "finance/personal_loan_comparison_calculator.html")
+
+
+def credit_utilization_calculator(request):
+    return render(request, "finance/credit_utilization_calculator.html")
+
+
+def debt_consolidation_savings_calculator(request):
+    return render(request, "finance/debt_consolidation_savings_calculator.html")
+
+
+def balance_transfer_calculator(request):
+    return render(request, "finance/balance_transfer_calculator.html")
+
+
+def student_loan_affordability_calculator(request):
+    return render(request, "finance/student_loan_affordability_calculator.html")
+
+
+def mortgage_affordability_calculator(request):
+    return render(request, "finance/mortgage_affordability_calculator.html")
+
+
+def car_affordability_calculator(request):
+    return render(request, "finance/car_affordability_calculator.html")
+
+
+def rent_affordability_calculator(request):
+    return render(request, "finance/rent_affordability_calculator.html")
+
+
+def no_spend_challenge_calculator(request):
+    return render(request, "finance/no_spend_challenge_calculator.html")
+
+
+def cash_envelope_system_planner(request):
+    return render(request, "finance/cash_envelope_system_planner.html")
+
+
+def biweekly_budget_converter(request):
+    return render(request, "finance/biweekly_budget_converter.html")
+
+
+def annual_budget_planner(request):
+    return render(request, "finance/annual_budget_planner.html")
+
+
+def sp500_historical_return_simulator(request):
+    return render(request, "finance/sp500_historical_return_simulator.html")
+
+
+def rule_of_72_calculator(request):
+    return render(request, "finance/rule_of_72_calculator.html")
+
+
+def rule_of_114_144_calculator(request):
+    return render(request, "finance/rule_of_114_144_calculator.html")
+
+
+def net_worth_projection_calculator(request):
+    return render(request, "finance/net_worth_projection_calculator.html")
