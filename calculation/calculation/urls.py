12920a523ab08637dbc207145063d8dbfa0152fc
M3 air <m3air@M3s-MacBook-Air.local>
2025-11-28 12:23:29 +0600

    add more calculator and fixed some

--- calculation/calculation/urls.py
+++ calculation/calculation/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('basic_calculator.urls')),
+    path('math/', include('basic_calculator.urls')),
     path('finance/', include('finance.urls')),
     path('health/', include('health.urls')),
 ]
