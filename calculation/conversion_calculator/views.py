f732ea3198318ff5105cccf400480cbc82aefce4
M3 air <m3air@M3s-MacBook-Air.local>
2026-01-31 17:56:42 +0600

    update on 31 Jan

--- calculation/conversion_calculator/views.py
+++ calculation/conversion_calculator/views.py
@@ -33,3 +33,195 @@ def conversion_dashboard(request):
 
 def length_converter(request):
     return render(request, 'conversion_calculator/length_converter.html')
+
+
+def weight_mass_converter(request):
+    return render(request, 'conversion_calculator/weight_mass_converter.html')
+
+
+def volume_converter(request):
+    return render(request, 'conversion_calculator/volume_converter.html')
+
+
+def area_converter(request):
+    return render(request, 'conversion_calculator/area_converter.html')
+
+
+def temperature_converter(request):
+    return render(request, 'conversion_calculator/temperature_converter.html')
+
+
+def speed_converter(request):
+    return render(request, 'conversion_calculator/speed_converter.html')
+
+
+def pressure_converter(request):
+    return render(request, 'conversion_calculator/pressure_converter.html')
+
+
+def energy_converter(request):
+    return render(request, 'conversion_calculator/energy_converter.html')
+
+
+def power_converter(request):
+    return render(request, 'conversion_calculator/power_converter.html')
+
+
+def data_storage_converter(request):
+    return render(request, 'conversion_calculator/data_storage_converter.html')
+
+
+def time_zone_converter(request):
+    return render(request, 'conversion_calculator/time_zone_converter.html')
+
+
+def cooking_measurement_converter(request):
+    return render(request, 'conversion_calculator/cooking_measurement_converter.html')
+
+
+def shoe_size_converter(request):
+    return render(request, 'conversion_calculator/shoe_size_converter.html')
+
+
+def clothing_size_converter(request):
+    return render(request, 'conversion_calculator/clothing_size_converter.html')
+
+
+def angle_converter(request):
+    return render(request, 'conversion_calculator/angle_converter.html')
+
+
+def fuel_consumption_converter(request):
+    return render(request, 'conversion_calculator/fuel_consumption_converter.html')
+
+
+def digital_image_resolution_converter(request):
+    return render(request, 'conversion_calculator/digital_image_resolution_converter.html')
+
+
+def lumber_board_foot_calculator(request):
+    return render(request, 'conversion_calculator/lumber_board_foot_calculator.html')
+
+
+def ring_size_converter(request):
+    return render(request, 'conversion_calculator/ring_size_converter.html')
+
+
+def number_system_converter(request):
+    return render(request, 'conversion_calculator/number_system_converter.html')
+
+
+def roman_numeral_converter(request):
+    return render(request, 'conversion_calculator/roman_numeral_converter.html')
+
+
+def morse_code_converter(request):
+    return render(request, 'conversion_calculator/morse_code_converter.html')
+
+
+def braille_converter(request):
+    return render(request, 'conversion_calculator/braille_converter.html')
+
+
+def password_generator_calculator(request):
+    return render(request, 'conversion_calculator/password_generator_calculator.html')
+
+
+def cryptographic_hash_generator(request):
+    return render(request, 'conversion_calculator/cryptographic_hash_generator.html')
+
+
+def color_code_converter(request):
+    return render(request, 'conversion_calculator/color_code_converter.html')
+
+
+def electric_charge_converter(request):
+    return render(request, 'conversion_calculator/electric_charge_converter.html')
+
+
+def radiation_dose_converter(request):
+    return render(request, 'conversion_calculator/radiation_dose_converter.html')
+
+
+def viscosity_converter(request):
+    return render(request, 'conversion_calculator/viscosity_converter.html')
+
+
+def flow_rate_converter(request):
+    return render(request, 'conversion_calculator/flow_rate_converter.html')
+
+
+def torque_converter(request):
+    return render(request, 'conversion_calculator/torque_converter.html')
+
+
+def density_converter(request):
+    return render(request, 'conversion_calculator/density_converter.html')
+
+
+def surface_tension_converter(request):
+    return render(request, 'conversion_calculator/surface_tension_converter.html')
+
+
+def catalytic_activity_converter(request):
+    return render(request, 'conversion_calculator/catalytic_activity_converter.html')
+
+
+def luminous_intensity_converter(request):
+    return render(request, 'conversion_calculator/luminous_intensity_converter.html')
+
+
+def radioactivity_converter(request):
+    return render(request, 'conversion_calculator/radioactivity_converter.html')
+
+
+def acceleration_converter(request):
+    return render(request, 'conversion_calculator/acceleration_converter.html')
+
+
+def force_converter(request):
+    return render(request, 'conversion_calculator/force_converter.html')
+
+
+def moment_of_inertia_converter(request):
+    return render(request, 'conversion_calculator/moment_of_inertia_converter.html')
+
+
+def metric_prefix_converter(request):
+    return render(request, 'conversion_calculator/metric_prefix_converter.html')
+
+
+def typography_unit_converter(request):
+    return render(request, 'conversion_calculator/typography_unit_converter.html')
+
+
+def audio_conversion_calculator(request):
+    return render(request, 'conversion_calculator/audio_conversion_calculator.html')
+
+
+def video_file_size_calculator(request):
+    return render(request, 'conversion_calculator/video_file_size_calculator.html')
+
+
+def screen_aspect_ratio_calculator(request):
+    return render(request, 'conversion_calculator/screen_aspect_ratio_calculator.html')
+
+
+def map_scale_calculator(request):
+    return render(request, 'conversion_calculator/map_scale_calculator.html')
+
+
+def earthquake_magnitude_scale_calculator(request):
+    return render(request, 'conversion_calculator/earthquake_magnitude_scale_calculator.html')
+
+
+def hurricane_speed_calculator(request):
+    return render(request, 'conversion_calculator/hurricane_speed_calculator.html')
+
+
+def ph_to_hydrogen_ion_concentration_converter(request):
+    return render(request, 'conversion_calculator/ph_to_hydrogen_ion_concentration_converter.html')
+
+
+def blood_sugar_unit_converter(request):
+    return render(request, 'conversion_calculator/blood_sugar_unit_converter.html')
--- calculation/real_estate_calculator/views.py
+++ calculation/real_estate_calculator/views.py
@@ -34,3 +34,151 @@ def real_estate_dashboard(request):
 
 def days_on_market_calculator(request):
     return render(request, 'real_estate_calculator/days_on_market_calculator.html')
+
+def mortgage_payment_calculator(request):
+    return render(request, 'real_estate_calculator/mortgage_payment_calculator.html')
+
+def piti_mortgage_calculator(request):
+    return render(request, 'real_estate_calculator/piti_mortgage_calculator.html')
+
+def mortgage_affordability_calculator(request):
+    return render(request, 'real_estate_calculator/mortgage_affordability_calculator.html')
+
+def rent_vs_buy_calculator(request):
+    return render(request, 'real_estate_calculator/rent_vs_buy_calculator.html')
+
+def mortgage_amortization_schedule_generator(request):
+    return render(request, 'real_estate_calculator/mortgage_amortization_schedule_generator.html')
+
+def extra_mortgage_payment_calculator(request):
+    return render(request, 'real_estate_calculator/extra_mortgage_payment_calculator.html')
+
+def bi_weekly_portgage_payment_calculator(request):
+    return render(request, 'real_estate_calculator/bi_weekly_portgage_payment_calculator.html')
+
+def refinance_breakeven_calculator(request):
+    return render(request, 'real_estate_calculator/refinance_breakeven_calculator.html')
+
+def dti_ratio_calculator(request):
+    return render(request, 'real_estate_calculator/dti_ratio_calculator.html')
+
+def ltv_ratio_calculator(request):
+    return render(request, 'real_estate_calculator/ltv_ratio_calculator.html')
+
+def down_payment_calculator(request):
+    return render(request, 'real_estate_calculator/down_payment_calculator.html')
+
+def pmi_calculator(request):
+    return render(request, 'real_estate_calculator/pmi_calculator.html')
+
+def fha_loan_calculator(request):
+    return render(request, 'real_estate_calculator/fha_loan_calculator.html')
+
+def va_loan_calculator(request):
+    return render(request, 'real_estate_calculator/va_loan_calculator.html')
+
+def usda_loan_calculator(request):
+    return render(request, 'real_estate_calculator/usda_loan_calculator.html')
+
+def arm_calculator(request):
+    return render(request, 'real_estate_calculator/arm_calculator.html')
+
+def closing_cost_calculator(request):
+    return render(request, 'real_estate_calculator/closing_cost_calculator.html')
+
+
+def seller_net_proceeds_calculator(request):
+    return render(request, 'real_estate_calculator/seller_net_proceeds_calculator.html')
+
+def capital_gains_tax_on_real_estate_calculator(request):
+    return render(request, 'real_estate_calculator/capital_gains_tax_on_real_estate_calculator.html')
+
+def rental_property_cash_flow_calculator(request):
+    return render(request, 'real_estate_calculator/rental_property_cash_flow_calculator.html')
+
+def gross_rent_multiplier_calculator(request):
+    return render(request, 'real_estate_calculator/gross_rent_multiplier_calculator.html')
+
+def capitalization_rate_calculator(request):
+    return render(request, 'real_estate_calculator/capitalization_rate_calculator.html')
+
+def cash_on_cash_return_calculator(request):
+    return render(request, 'real_estate_calculator/cash_on_cash_return_calculator.html')
+
+def total_return_on_investment_calculator(request):
+    return render(request, 'real_estate_calculator/total_return_on_investment_calculator.html')
+
+def debt_service_coverage_ratio_calculator(request):
+    return render(request, 'real_estate_calculator/debt_service_coverage_ratio_calculator.html')
+
+def rule_quickrental_analysis_calculator(request):
+    return render(request, 'real_estate_calculator/rule_quickrental_analysis_calculator.html')
+
+def rule_house_flipping_calculator(request):
+    return render(request, 'real_estate_calculator/rule_house_flipping_calculator.html')
+
+def property_tax_calculator(request):
+    return render(request, 'real_estate_calculator/property_tax_calculator.html')
+
+def homeowners_insurance_premium_estimator(request):
+    return render(request, 'real_estate_calculator/homeowners_insurance_premium_estimator.html')
+
+def home_appreciation_value_projection_calculator(request):
+    return render(request, 'real_estate_calculator/home_appreciation_value_projection_calculator.html')
+
+def price_per_square_foot_calculator(request):
+    return render(request, 'real_estate_calculator/price_per_square_foot_calculator.html')
+
+def real_estate_agent_commission_calculator(request):
+    return render(request, 'real_estate_calculator/real_estate_agent_commission_calculator.html')
+
+def land_lease_payment_calculator(request):
+    return render(request, 'real_estate_calculator/land_lease_payment_calculator.html')
+
+def piti_as_percentage_of_income_calculator(request):
+    return render(request, 'real_estate_calculator/piti_as_percentage_of_income_calculator.html')
+
+def balloon_mortgage_calculator(request):
+    return render(request, 'real_estate_calculator/balloon_mortgage_calculator.html')
+
+def construction_loan_interest_calculator(request):
+    return render(request, 'real_estate_calculator/construction_loan_interest_calculator.html')
+
+def home_equity_loan_calculator(request):
+    return render(request, 'real_estate_calculator/home_equity_loan_calculator.html')
+
+def heloc_calculator(request):
+    return render(request, 'real_estate_calculator/heloc_calculator.html')
+
+def reverse_mortgage_calculator(request):
+    return render(request, 'real_estate_calculator/reverse_mortgage_calculator.html')
+
+def buy_to_let_mortgage_calculator(request):
+    return render(request, 'real_estate_calculator/buy_to_let_mortgage_calculator.html')
+
+def hoa_fee_affordability_calculator(request):
+    return render(request, 'real_estate_calculator/hoa_fee_affordability_calculator.html')
+
+def moving_cost_estimator(request):
+    return render(request, 'real_estate_calculator/moving_cost_estimator.html')
+
+def home_repair_budget_calculator(request):
+    return render(request, 'real_estate_calculator/home_repair_budget_calculator.html')
+
+def depreciation_calculator(request):
+    return render(request, 'real_estate_calculator/depreciation_calculator.html')
+
+def exchange_calculator(request):
+    return render(request, 'real_estate_calculator/exchange_calculator.html')
+
+def annual_property_operating_data_calculator(request):
+    return render(request, 'real_estate_calculator/annual_property_operating_data_calculator.html')
+
+def net_operating_income_calculator(request):
+    return render(request, 'real_estate_calculator/net_operating_income_calculator.html')
+
+def internal_rate_of_return_calculator(request):
+    return render(request, 'real_estate_calculator/internal_rate_of_return_calculator.html')
+
+def tenant_rent_to_income_ratio_calculator(request):
+    return render(request, 'real_estate_calculator/tenant_rent_to_income_ratio_calculator.html')
